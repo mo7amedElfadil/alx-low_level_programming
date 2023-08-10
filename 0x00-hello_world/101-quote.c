b791580d510c0b629caeff748e56a5fb9ece0f91
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-08-10 23:16:14 +0300

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Using write function to output string
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	ssize_t write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",59);
+}	
+
