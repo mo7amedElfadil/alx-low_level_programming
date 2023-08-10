9b67e5f61c0d67b2d8bd1c649979388898e6606d
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-08-10 21:50:20 +0300

    4-puts.c - making it betty appropriate

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-
-int main(void){
+/**
+ * main - Entry point
+ * Using puts function to output string
+ * Return Always 0 (Success)
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
