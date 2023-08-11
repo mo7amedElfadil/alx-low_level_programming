8e3c6c089a0710d521b20c273ec3614237bc63f4
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-08-12 00:28:56 +0300

    t12 refactored to match betty criteria

---
+++ 0x01-variables_if_else_while/102-backup_first_approach.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	i = 48;
+	while (i <= 57)
+	{
+		j = 48;
+		while (j <= 57)
+		{
+			k = i;
+			while (k <= 57)
+			{
+				l = 48;
+				while (l <= 57)
+				{
+					if ((k >= i) && !((i >= k) && (j >= l)))
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(k);
+						putchar(l);
+						if (!((i == 57) && (j == 56)))
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+					l++;
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}	
+	putchar('\n');
+	return (0);
+}
