f5bb0789344a216e1e49373f9c9fec05ce027bf6
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-08-11 19:07:12 +0300

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	alphabet = 'a';
+	while (alphabet <= 'z')
+	{
+		if (alphabet == 'e' || alphabet == 'q') 
+		{
+			alphabet++;
+			continue;
+		}
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
