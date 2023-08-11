e94712475aa636c015029ba13b78c02057f65be7
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-08-11 20:08:27 +0300

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 47;
+	while (i++ < 57)
+	{
+		putchar(i);
+		if (i < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
