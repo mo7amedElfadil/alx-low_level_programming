01d361a0bdf787b47c33fc948e89ae02d106e8b6
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-08-15 17:40:32 +0300

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: integer to test for polarity
+ * Return: 1 and print + if n > 0
+ *		   0 and print 0 if n == 0
+ *		   -1 and print - if n < 0
+ */
+int print_sign(int n)
+{
+	if ( n > 0 )
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if ( n == 0 )
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
