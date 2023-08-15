85f9406ad84056ba9cfdc326ea1e451170f0b3dc
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-08-16 01:25:40 +0300

    04-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entrypoint. computes and prints the first
+ *		  50 fibonacci numbers starting with 1 & 2
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	unsigned long a = 1, b = 2, c;
+
+	printf("%lu", a);
+	printf(", %lu", b);
+
+
+	while (i < 96)
+	{
+		c = a + b;
+		printf(", %lu", c);
+		a = b;
+		b = c;
+
+
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
