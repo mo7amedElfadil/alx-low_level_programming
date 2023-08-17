febce62f452d59bf1021c400990ee0ced9ac3872
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-08-17 19:40:04 +0300

    t3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_numbers - print all single digit no. 0-9
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+		_putchar(c);
+	_putchar(10);
+}
