43dc0130b77cfed50db93bae3dce8826baec43f5
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-08-17 20:52:32 +0300

    t10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * print_triangle - draws a triangle on the terminal.
+ * @size: integer to determine the size of the triangle
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i; j++)
+		{
+			_putchar(32);
+		}
+		for (j = size - i; j <= size; j++)
+		{
+			_putchar(35);
+		}
+
+		_putchar(10);
+
+	}
+	if (size <= 0)
+		_putchar(10);
+}
