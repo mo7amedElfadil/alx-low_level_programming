789066faffe06156ed0edffbc4150aa3ca2ac0bf
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-08-18 04:03:38 +0300

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_number - Function that prints an integer
+ * @n: integer to be printed
+ * Return: Always 0 (Success)
+ */
+void print_number(int n)
+{
+	int size = 1;
+
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	while (size < n / 10)
+	{
+		size *= 10;
+	}
+	while (size >= 1)
+	{
+		_putchar('0' + n / size);
+		n %= size;
+		size /= 10;
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -12,4 +12,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 int is_prime(int p);
+void print_number(int n);
 #endif
