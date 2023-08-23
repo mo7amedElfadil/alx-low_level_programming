3db8dc6d88e9486f539ecf1fd1070c082b013cd9
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-08-23 22:55:28 +0300

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  * cap_string - capitalize all words of a string
- * Separators of words: space, tabulation, new line, ,, ;, ., !, ?, ", (, ), {, and }
+ * Separators of words: space, tabulation, new line,
+ *				,, ;, ., !, ?, ", (, ), {, and }
  *		32 9 13 44 59 46 33 63 34 40 41 123 125
  * @s: string to capitalize
  * Return: Always 0 (Success)
@@ -9,11 +10,11 @@
 char *cap_string(char *s)
 {
 	int i = 0, j;
-	int sep[13] ={32, 9, 13, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int sep[13] = {32, 9, 13, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	while (*(s + i) != '\0')
 	{
-		if (*(s + i) >= 97 && *(s + i) <= 122 )
+		if (*(s + i) >= 97 && *(s + i) <= 122)
 		{
 			for (j = 0; j < 13; j++)
 				if (*(s + i - 1) == sep[j])
