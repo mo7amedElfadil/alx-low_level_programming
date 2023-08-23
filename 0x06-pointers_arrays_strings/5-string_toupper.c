ca4f4d00d8218c0f4468051c44b50a88f6961fae
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-08-23 22:33:09 +0300

    5-string_toupper.c fix

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,17 +2,18 @@
 /**
  * string_toupper - convert all lowercase letters of a
  *				string to uppercase
- *
+ * @s: string to convert to uppercase
  * Return: Always 0 (Success)
  */
 char *string_toupper(char *s)
 {
-	while ( *s != '\0')
-	{
-		if (*s >= 97 && *s <=122)
-			*s-= 32;
+	int i = 0;
 
-		*s++;
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) >= 97 && *(s + i) <= 122)
+			*(s + i) -= 32;
+		i++;
 	}
 
 
