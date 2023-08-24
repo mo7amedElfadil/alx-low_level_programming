98479bd1e5ff7d5160ed5b04aeae57398d2a83b4
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-08-24 17:17:59 +0300

    7-leet.c using 2 arrays

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,11 +1,6 @@
 #include "main.h"
 /**
  * leet - function that encodes a string into 1337.
- *		Letters a and A should be replaced by 4	97 65 52
- *		Letters e and E should be replaced by 3	101 69 51
- *		Letters o and O should be replaced by 0	111 79 48
- *		Letters t and T should be replaced by 7	116 84	55
- *		Letters l and L should be replaced by 1	108 76 49
  *		can only use one if and 2 loops
  *		cant use switch
  * @s: string to encode
