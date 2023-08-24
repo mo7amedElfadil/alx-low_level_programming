bcfb3ec685b195d922b1500a5b4e218edc896f51
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-08-24 23:16:47 +0300

    104-print_buffer.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_buffer - prints a buffer
+ * @b: pointer to the buffer
+ * @size: size in bytes of the buffer pointed by b
+ */
+void print_buffer(char *b, int size)
+{
+
+
+}
