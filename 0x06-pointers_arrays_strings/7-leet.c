7c5658bd2b04221ff66217ba8aaafe6a8b0514b4
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-08-24 17:19:29 +0300

    7-leet.c using 2 arrays

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,16 +1,16 @@
 #include "main.h"
+
 /**
  * leet - function that encodes a string into 1337.
- *		can only use one if and 2 loops
- *		cant use switch
  * @s: string to encode
- * Return: s
+ * Return: pointer to s
  */
+
 char *leet(char *s)
 {
 	int i = 0, j;
 	char nums[] = "4433007711";
-	char let[] =	"aAeEoOtTlL";
+	char let[] = "aAeEoOtTlL";
 
 	while (s[i] != '\0')
 	{
