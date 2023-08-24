466fa022d674bfde8448a6e9556c6a883b42de90
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-08-24 18:24:12 +0300

    t6 cap first 1

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,7 +16,7 @@ char *cap_string(char *s)
 	{
 		for (j = 0; j < 13; j++)
 		{
-			if ((*(s + i - 1) == sep[j] || (i == 0) ))
+			if ((*(s + i - 1) == sep[j] || (i == 0)))
 				if (*(s + i) >= 97 && *(s + i) <= 122)
 				{
 					*(s + i) -= 32;
