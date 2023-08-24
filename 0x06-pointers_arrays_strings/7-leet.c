a59b15f5d9bb93409ab85d3fdd9abb17dbe83410
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-08-24 17:20:55 +0300

    7-leet.c using 2 arrays

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,22 +6,17 @@
  */
 char *leet(char *s)
 {
-	int i = 0, j;
-	char nums[] = "4433007711";
-	char let[] = "aAeEoOtTlL";
+	int i = 0, j = -1;
+	int let[10] = {65, 52, 69, 51, 79, 48, 84, 55, 76, 49};
 
-	while (s[i] != '\0')
+	while (*(s + i) != '\0')
 	{
 		j = 0;
-		while (let[j] != '\0')
+		while (j < 10)
 		{
-			if (s[i] == let[j])
-			{
-				s[i] = nums[j];
-				j++;
-				break;
-			}
-			j++;
+			if (*(s + i) == let[j] || *(s + i) - 32 == let[j])
+				*(s + i) = let[j + 1];
+			j += 2;
 		}
 		i++;
 	}
