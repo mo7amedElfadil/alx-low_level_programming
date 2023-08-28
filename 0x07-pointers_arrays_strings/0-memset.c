5d3021a0ef4b2099148aad3beaaa6291dc0378ad
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-08-28 18:50:44 +0300

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,12 +8,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
-	char **ptr = &s;
+	unsigned int i;
+	char *ptr = s;
 
 	for (i = 0; i < n; i++, ptr++)
 	{
-		**ptr = b;
+		*ptr = b;
 	}
 
 	return (s);
