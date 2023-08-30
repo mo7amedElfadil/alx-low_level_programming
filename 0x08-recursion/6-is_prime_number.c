b82953f32599bd0aeae6f216d124872e4978d95e
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-08-30 20:00:51 +0300

    t6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,15 +8,13 @@
 int is_prime_number(int n)
 {
 	int i;
+
 	if (n <= 1)
 		return (0);
-	else
-	{
-		for (i = 2; i < n; i++)
-			if(n % i == 0)
-				return (0);
-		return (1);
-	}
+	for (i = 2; i < n; i++)
+		if (n % i == 0)
+			return (0);
+	return (1);
 }
 
 
