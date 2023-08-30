404f2a1b7fd17e8e7fc970539b94a60f3975220b
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-08-30 22:45:59 +0300

    101-wildcmp.c pushing to save progress

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * wildcmp -  that compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: 1 if the strings can be considered identical
+ *		   0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if ((*s1 == *s2 && !(*s2)))
+		return (1);
+	else if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	else if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1));
+	else if (*s1 && *(s2 - 1) == '*')
+		return (wildcmp(s1 + 1, s2));
+	else
+		return (0);
+
+}
+
+
