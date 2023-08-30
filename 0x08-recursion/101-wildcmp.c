3a053179efd0cb52d9194686e044b83121288184
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-08-30 23:09:43 +0300

    101-wildcmp.c updated to solve condition of match when * is in the middle of a str

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -13,12 +13,16 @@ int wildcmp(char *s1, char *s2)
 	else if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	else if (*s2 == '*')
-		return (wildcmp(s1, s2 + 1));
+	{
+		if (*(s2 + 1) =='*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2 ))
+			return (1);
+	}
 	else if (*s1 && *(s2 - 1) == '*')
 		return (wildcmp(s1 + 1, s2));
-	else
-		return (0);
 
+	return (0);
 }
 
 
