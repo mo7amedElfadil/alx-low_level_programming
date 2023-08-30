6a57a47286ed7e8ca4e189ba3b806eec21b47694
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-08-30 17:25:21 +0300

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string find length of
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (!(*s))
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+
+}
