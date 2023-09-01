1ba523d71eaab4ad28ffdb58446ca6334c5fb6fc
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-09-01 16:45:14 +0300

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc: argument count (number of arguments passed to the program)
+ * @argv: argument vector (1-D array of argument strings
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[--argc]);
+
+	return (0);
+}
