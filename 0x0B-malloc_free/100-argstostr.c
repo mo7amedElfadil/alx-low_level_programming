41a428d921c4f89ab5b4d45a007ab1ca7f8ec0cb
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-09-04 22:42:25 +0300

    attempt to calculate str length manually

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,7 +8,7 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int i, sum = 0;
+	int sum = 0, i, j;
 	char *ptr = NULL;
 
 	if (!av || ac == 0)
@@ -16,35 +16,31 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 	}
 
-
-	for  (i = 0; i < ac; i++)
+	for (i = 0; i < ac; i++)
 	{
-		sum += _strlen_recursion(av[i]) + 1;
-		ptr = (char *)realloc(ptr, sum * sizeof(char));
-
-		if (!ptr)
+		j = 0;
+		while (av[i][j])
+		{
+			sum++;
+			j++;
+		}
+		sum++;
+	}
+	sum++;
+	ptr = (char *)realloc(ptr, sum * sizeof(*ptr));
+	if (!ptr)
 		{
 			free(ptr);
 			return (NULL);
 		}
+	for  (i = 0; i < ac; i++)
+	{
 		_strcat(ptr, av[i]);
 		_strcat(ptr, "\n");
 	}
 	return (ptr);
 }
 
-/**
- * _strlen_recursion - returns the length of a string
- * @s: string find length of
- * Return: length of string
- */
-int _strlen_recursion(char *s)
-{
-	if (!(*s))
-		return (0);
-	return (1 + _strlen_recursion(s + 1));
-
-}
 /**
  * _strcat - function that concatenates two strings
  *			by appending the src string to the dest string,
