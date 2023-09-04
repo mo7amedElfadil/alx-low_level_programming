fcb6fe078e4f1397dcd51ae8108d0b8f0a297447
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-09-04 15:20:21 +0300

    updating 0-create_array.c with size = 0 condition

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	arr = malloc(size * sizeof(c));
-	if (arr == NULL)
+	if (arr == NULL || size == 0)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		arr[i] = c;
