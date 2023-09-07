0cc264034ab157f3d1249662a6a0f3764c6cf7c6
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-09-07 20:45:45 +0300

    2-calloc.c fix4 splitting null conditions

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,10 +11,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *arr;
 	unsigned int i;
 
-	arr = malloc(size * nmemb);
-	if (!arr || size == 0 || nmemb == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-	for (i = 0;i < nmemb * size; i++)
+	arr = malloc(size * nmemb);
+	if (!arr)
+		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
 		arr[i] = 0;
 	return (arr);
 }
