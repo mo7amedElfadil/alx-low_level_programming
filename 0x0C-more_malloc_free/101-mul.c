0d9ddc94f9366a8477629fbc1a16bdee7f20aaa2
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-09-08 05:53:16 +0300

    idk man

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,8 +2,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string find length of
+ * Return: length of string
+ */
+unsigned int _strlen_recursion(char *s)
+{
+	if (!(*s))
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 
-int main(int argc, char **argv)
+}
+
+
+/**
+ * _isdigit - checks if a character is a digit
+ * @c: Character to be tested
+ * Return:  1 (Success)
+ *			0 (Failure)
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
+
+
+/**
+ * reverse_str - function that reverses an array.
+ * @a: pointer to an array.
+ * @n: number of elements in the array.
+ */
+void reverse_str(char *a, int n)
+{
+	int i = 0, temp;
+
+
+	while (i < n / 2)
+	{
+		temp = *(a + i);
+		*(a + i) = *(a + n - i - 1);
+		*(a + n - i - 1) = temp;
+		i++;
+	}
+}
+/**
+ * arg_test - tests arguments for certain conditions
+ *			  1. That 3 arguments were passed (including the name of the file)
+ *			  2. that the 2nd and 3rd arguments were all made up of digits
+ * @argc: number of arguments.
+ * @argv: pointer to argument array.
+ */
+
+void arg_test(int argc, char **argv)
 {
 	int i, j;
 
@@ -11,10 +65,9 @@ int main(int argc, char **argv)
 	{
 		printf("Error\n");
 		exit(98);
-		return(1);
 	}
 
-	for (i = 1; i < argc ; i++)
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j]; j++)
 		{
@@ -22,10 +75,86 @@ int main(int argc, char **argv)
 			{
 				printf("Error\n");
 				exit(98);
-				return (1);
 			}
 		}
 	}
+}
+/**
+ * _print_rev_recursion - prints a string in reverse, followed by a new line
+ * @s: string to be printed
+ */
+void _print_rev_recursion(char *s)
+{
+	if (!(*s))
+		return;
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 
+
+}
+/*
+ * print_output - prints and calculates the output
+ * @argv: arguemnt vector
+ * Return: 1 Failure
+ *			0 success
+ */
+int print_output(char **argv)
+{
+	char *result = NULL, *x, *y;
+	int i, j, k, size1, size2, carry = 0, c2, res, len;
+
+	x = argv[1], y = argv[2];
+	size1 = _strlen_recursion(x);
+	size2 = _strlen_recursion(y);
+	len = size1 + size2 + 1;
+	result = malloc(sizeof(char) * len);
+	if (!result)
+	{
+		return (1);
+	}
+	for (i = 0; i < len; i++)
+		result[i] = 0;
+	reverse_str(x, size1), reverse_str(y, size2);
+	for (i = 0; i < size1; i++)
+	{
+		carry = 0;
+		for (j = 0; j < size2; j++)
+		{
+			carry += ((x[i] - 48) * (y[j] - 48)) + result[i + j];
+			result[i + j] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[i + j] += carry;
+	}
+
+	for (k = 0; k < len - 1; k++)
+		result[k] += 48;
+	_print_rev_recursion(result);
+	putchar('\n');
+	free(result);
 	return (0);
+}
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+
+	arg_test(argc, argv);
+	return (print_output(argv));
 }
