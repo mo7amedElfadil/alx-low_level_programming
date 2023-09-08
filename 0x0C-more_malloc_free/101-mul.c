f9769f1241dc91abc03659190506c629533e5fd3
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-09-08 05:39:41 +0300

    made a separate function

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -158,6 +158,26 @@ int _putchar(char c)
  */
 int main(int argc, char **argv)
 {
+	int i, j;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	for (i = 1; i < argc ; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (!(_isdigit(argv[i][j])))
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+
 	arg_test(argc, argv);
 	return (print_output(argv));
 }
