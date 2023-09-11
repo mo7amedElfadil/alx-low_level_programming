0be8eeea844054eeb7113a9d7785c15463d56de6
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-09-11 18:02:48 +0300

    attempted to allocate memory for name and owner

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,21 @@
 #include "main.h"
 #include "dog.h"
 /**
+ * _strlen_recursion - returns the length of a string
+ * @s: string find length of
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (!(*s))
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+
+}
+
+
+
+/**
  * new_dog - creates a new struct instance of dog using the  dog_t type def
  * @name: name of dog
  * @age: age of dog
@@ -11,13 +26,28 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	int len_n, len_o;
 
 	d = malloc(sizeof(dog_t));
 	if (!d)
 		return (NULL);
+	len_n = _strlen_recursion(name) + 1;
+	d->name = malloc(sizeof(*name) * len_n);
+	if (!d->name)
+		{
+			free(d);
+			return(NULL);
+		}
 	d->name = name;
-	d->age = age;
-	d->owner = owner;
 
+	len_o = _strlen_recursion(name) + 1;
+	d->owner = malloc(sizeof(*name) * len_o);
+	if (!d->owner)
+		{
+			free(d);
+			return(NULL);
+		}
+	d->owner = owner;
+	d->age = age;
 	return (d);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,4 +15,5 @@ typedef struct dog
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
+int _strlen_recursion(char *s);
 #endif
