3aca0218ad081e1f1aa230ccca8697110720c3fa
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-09-11 17:28:41 +0300

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,7 +10,6 @@ void print_dog(struct dog *d)
 
 	if (d)
 	{
-
 		printf("Name: %s\n", (d->name ? d->name : nil));
 		(d->age ? printf("Age: %f\n", d->age) : printf("Age: %s\n", nil));
 		printf("Owner: %s\n", (d->owner ? d->owner : nil));
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - struct for a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
 typedef struct dog
 {
 	char *name;
