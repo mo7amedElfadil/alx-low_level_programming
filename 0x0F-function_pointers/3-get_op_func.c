4fb2d4b8795d793a4f587d719ebe00f5b192ccc3
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-09-13 18:35:12 +0300

    used strcmp in get_op_func

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,9 +20,9 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (ops[i].op != NULL)
 	{
-		if (*ops[i].op == *s)
+		if (!strcmp(ops[i].op ,s))
 			return (ops[i].f);
 		i++;
 	}
-	return (ops[i].f);
+	return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,13 +8,14 @@
 int main(int argc, char **argv)
 {
 	int result;
-	int (*ptr)(int, int) = (get_op_func)(argv[2]);
+	int (*ptr)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	ptr = (get_op_func)(argv[2]);
 	if (!ptr)
 	{
 		printf("Error\n");
@@ -26,6 +27,7 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(100);
 	}
+
 	result = ptr(atoi(argv[1]), atoi(argv[3]));
 	printf("%d\n", result);
 	return (0);
