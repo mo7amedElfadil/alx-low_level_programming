4c0d7b886c357e135d9c5d5be1f0757422996f48
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-09-13 18:23:21 +0300

    calculator function first commit

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+/**
+ * main - check the code
+ * @argc: arument count
+ * @argv: argument vector
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	int result;
+	int (*ptr)(int, int) = (get_op_func)(argv[2]);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (!ptr)
+	{
+		printf("Error\n");
+		exit(99);
+
+	}
+	if ((strcmp(argv[2], "/") || strcmp(argv[2], "%")) && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	result = ptr(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", result);
+	return (0);
+}
+
+
