950f59d8ee748de601e9d67c1f08e3e69c597568
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-09-13 19:14:14 +0300

    100-main_opcodes.c update optcodes printed

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,13 +2,16 @@
 #include <stdlib.h>
 /**
  * main - Entry point
- *
+ * @argc: argument counter
+ * @argv: argument vector
  * Return: Always 0 (Success)
  */
 int main(int argc, char **argv)
 {
 
-	int n;
+	int i, n;
+	int (*main_ptr)(int, char **) = main;
+	unsigned char *optcodes = (unsigned char *)main_ptr;
 
 	if (argc != 2)
 	{
@@ -21,7 +24,13 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(2);
 	}
-
+	for (i = 0; i < n; i++)
+	{
+		printf("%02x", optcodes[i]);
+		if (i < n - 1)
+			putchar(' ');
+	}
+	putchar('\n');
 
 	return (0);
 }
