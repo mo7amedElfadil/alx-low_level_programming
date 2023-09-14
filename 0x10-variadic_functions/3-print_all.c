939d07b54c1ef03d51eb6d3097e575e916aeeb5a
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-09-14 21:29:18 +0300

    betty + added flag for separator condition

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,51 +2,43 @@
 
 /**
  * print_all - prints anything.
- * @format:  list of types of arguments passed to the function
+ * @format: list of types of arguments passed to the function
  *
- * @n: number of parameters
  */
 void print_all(const char * const format, ...)
 {
-	int i;
+	int i, flag;
 	va_list ptr;
 	char *s;
 
-	va_start(ptr,format);
+	va_start(ptr, format);
 	i = 0;
-	while(format && format[i])
+	while (format && format[i])
 	{
-		switch(format[i])
+		flag = 1;
+		switch (format[i])
 		{
 			case 'c':
-				printf("%c",va_arg(ptr, int));
+				printf("%c", va_arg(ptr, int));
 				break;
 			case 'i':
-				printf("%d",va_arg(ptr, int));
-
+				printf("%d", va_arg(ptr, int));
 				break;
 			case 'f':
-				printf("%f",va_arg(ptr, double));
-
+				printf("%f", va_arg(ptr, double));
 				break;
 			case 's':
 				s = va_arg(ptr, char *);
-				printf("%s",(s
-							? s
-							: "(nil)"
-							));
+				printf("%s", (s ? s	: "(nil)"));
 				break;
 			default:
+				flag = 0;
 				break;
-
 		}
-		if (i <(int)(strlen(format) - 1))
+		if (i < (int)(strlen(format) - 1) && flag)
 			printf(", ");
-
-
 		i++;
 	}
 	printf("\n");
 	va_end(ptr);
 }
-
