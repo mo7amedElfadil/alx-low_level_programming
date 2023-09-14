9bcec9f41578eb83418264f87bbf47303ad315a7
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-09-14 04:01:03 +0300

    fix

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
 
 	int i, n;
 
-	char *(main_ptr) = (char *)main;
+	unsigned char *(main_ptr) = (unsigned char *)main;
 
 	if (argc != 2)
 	{
@@ -28,7 +28,7 @@ int main(int argc, char **argv)
 	}
 	for (i = 0; i < n; i++)
 	{
-		printf("%02hhx", main_ptr[i]);
+		printf("%02x", main_ptr[i]);
 		if (i < n - 1)
 			putchar(' ');
 	}
