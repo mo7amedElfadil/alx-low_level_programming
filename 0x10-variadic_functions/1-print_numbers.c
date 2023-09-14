0981762cc62f266c9f3c53aeceb4f2f867025ea5
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-09-14 18:27:15 +0300

    placed condition in brackets for type cast

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,7 +23,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < (int)n; i++)
 	{
 		printf("%d%s", va_arg(ptr, int),
-			((i != (int)n - 1 && separator) ?
+			((i != (int)(n - 1) && separator) ?
 			 separator : ""));
 	}
 
