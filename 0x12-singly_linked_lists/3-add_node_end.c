a848fa5625ceb4a162604c1183169f47da6f5cc3
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-09-21 18:21:20 +0300

    4-free_list.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -18,16 +18,9 @@ list_t *add_node_end(list_t **head, const char *str)
 		if (new == NULL)
 			return (NULL);
 
-		if (str)
-		{
-			new->str = strdup(str);
-			new->len = _strlen(new->str);
-		}
-		else
-		{
-			new->str = NULL;
-			new->len = 0;
-		}
+
+		new->str = strdup(str);
+		new->len = _strlen(new->str);
 		new->next = NULL;
 		(*head) = new;
 	}
