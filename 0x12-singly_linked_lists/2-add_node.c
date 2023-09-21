2cc54cabed3c4ef8d3990ae9c48ecaa72d3c4c8e
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-09-22 01:17:06 +0300

    added extra head = new

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,9 +11,10 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = NULL;
 
-	new = malloc(sizeof(list_t));
 	if (*head == NULL)
 		*head = new;
+
+	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
