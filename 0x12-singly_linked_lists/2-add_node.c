5f450d2bbd35c1bd85f6959f6a622418130f7772
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-09-21 17:00:58 +0300

    2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a node at the beginning of a list_t list
+ * @head: ptr to head ptr of the linked list
+ * @str: string to be duplicated into the node's str member
+ * Return:  address of a new element or null if failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = _strlen(new->str);
+	new->next = *head;
+	*head = new;
+	return (new);
+
+
+
+}
+
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: pointer to a string.
+ * Return: length of string s.
+ */
+int _strlen(char *s)
+{
+	int  l = 0;
+
+	while (s[l])
+		l++;
+	return (l);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -22,4 +22,9 @@ typedef struct list_s
 /* Function prototypes */
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+
+/* Utility Functions */
+int _strlen(char *s);
+
 #endif
