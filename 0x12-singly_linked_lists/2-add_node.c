1b88c61368141cae97b1f2d93ec9df6dce57fd32
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-09-22 01:14:51 +0300

    moved head = new above malloc

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,12 +12,13 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new = NULL;
 
 	new = malloc(sizeof(list_t));
+	if (*head == NULL)
+		*head = new;
 	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
 	new->len = strlen(new->str);
 	new->next = (*head);
-	(*head) = new;
 	return (*head);
 }
 
