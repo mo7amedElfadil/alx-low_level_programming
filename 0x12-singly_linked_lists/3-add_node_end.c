0d79f1db8351fe8a7644f57932f1d3b8de68806c
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-09-21 17:46:44 +0300

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,28 +1,35 @@
 #include "lists.h"
 
 /**
- * add_node - adds a node at the beginning of a list_t list
+ * add_node_end - adds a node at the beginning of a list_t list
  * @head: ptr to head ptr of the linked list
  * @str: string to be duplicated into the node's str member
  * Return:  address of a new element or null if failed
  */
 
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
+	if (*head == NULL)
+	{
+		list_t *new;
 
-	if (str)
+		new = malloc(sizeof(list_t));
+		if (new == NULL)
+			return (NULL);
+
 		new->str = strdup(str);
+		new->len = _strlen(new->str);
+		new->next = NULL;
+		(*head) = new;
+	}
 	else
-		new->str = NULL;
-	new->len = _strlen(new->str);
-	new->next = (*head);
-	(*head) = new;
-	return (new);
+	{
+		(*head)->next = (add_node_end(&(*head)->next, str));
+	}
+
+
+	return (*head);
 
 
 
