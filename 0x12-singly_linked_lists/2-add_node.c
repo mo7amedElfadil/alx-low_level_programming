d711a5faf132a6f0167dc425dbb617dd25586000
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-09-22 01:19:42 +0300

    used _strlen of str

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,17 +11,26 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = NULL;
 
-	if (*head == NULL)
-		*head = new;
-
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
-	new->len = strlen(new->str);
+	new->len = _strlen(str);
 	new->next = (*head);
 	(*head) = new;
 	return (*head);
 }
 
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: pointer to a string.
+ * Return: length of string s.
+ */
+int _strlen(const char *s)
+{
+	int  l = 0;
 
+	while (s[l])
+		l++;
+	return (l);
+}
