d205b3698087607b8f02188c5cb1a77d47914d01
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-09-25 18:53:05 +0300

    bettify 7-get_nodeint.c and removed redundant conditions

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,23 +1,18 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index
- * head: head of the linked list to be printed
- * index: index of list
- * Return: node at index
- * */
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list.
+ * @head: head of the linked list to be printed
+ * @index: index of list
+ * Return: node at index or NULL if failed
+ */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	if (!head)
 		return (NULL);
 	if (index != 0)
-	{
-		if (head->next)
-			return (get_nodeint_at_index(head->next, index - 1));
-		else
-			return (NULL);
-	}
+		return (get_nodeint_at_index(head->next, index - 1));
 	return (head);
 
 
