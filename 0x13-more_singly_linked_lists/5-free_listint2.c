3fb64d547d2b2f4f1f8c3fc2acd952558d21d8d6
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-09-25 18:22:11 +0300

    fix 5-free_listint2.c null check for head and *head

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,12 +6,11 @@
  */
 void free_listint2(listint_t **head)
 {
-	if (*head != NULL)
+	if (head || *head)
 	{
 		(free_listint2(&(*head)->next));
 		free((*head));
 	}
 	*head = NULL;
-	head = NULL;
 
 }
