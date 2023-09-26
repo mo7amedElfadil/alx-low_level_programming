7b468ed28c7cc70be544c6d5314ad88c4ea1a0d2
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-09-26 03:13:52 +0300

    fix 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,33 +11,46 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	listint_t *new = NULL;
+
 	if (!head)
 		return (NULL);
 
-	if (idx - 1 && (*head))
+	if (!idx && !*head)
+	{
+		return (add_nodeint(head, n));
+	}
+	else if (idx - 1 && (*head))
 	{
 		return (insert_nodeint_at_index(&(*head)->next, idx - 1, n));
 	}
-	else
+	else if (!idx)
 	{
-		listint_t *new = NULL;
-
-		new = malloc(sizeof(listint_t));
-		if (new == NULL)
-			return (NULL);
-		new->n = n;
-		if ((*head)->next)
-		{
-			new->next = (*head);
-			(*head) = new;
-		}
-		else
-		{
-			new->next = NULL;
-			(*head) = new;
-		}
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = (*head)->next;
+	(*head)->next = new;
 	}
-	return ((*head)->next);
+	return (new);
+}
+/**
+ * add_nodeint - adds a node at the beginning of a listint_t list
+ * @head: ptr to head ptr of the linked list
+ * @n: integer value to be assigned to node
+ * Return:  address of a new element or null if failed
+ */
 
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new = NULL;
 
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = (*head);
+	(*head) = new;
+	return (*head);
 }
