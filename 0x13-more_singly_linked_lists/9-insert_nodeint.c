e887913d5fe9a705cdc2a527a7be1ee0246db8cf
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-09-26 03:42:09 +0300

    9x

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (!head)
 		return (NULL);
-	if (!*head && !idx)
+	if (!idx)
 		return (add_nodeint(head, n));
 
 	if (idx > 0)
