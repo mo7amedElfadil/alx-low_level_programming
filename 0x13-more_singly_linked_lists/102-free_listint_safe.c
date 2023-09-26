ee5d7fa0af19c8c92ff8fdba7e1207d644d7ad46
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-09-27 00:37:33 +0300

    102-free_listint_safe.c attempt 1

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - free all elements of a listint_t list safely
+ * @h: head of the linked list to be printed
+ * Return: size of list
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *node = NULL;
+	size_t i = 0;
+
+	if (!(h) || !(*h))
+		return (0);
+	while (*h)
+	{
+		node = (*h)->next;
+		if (*h - node > 0)
+		{
+			free(*h);
+			*h = node;
+			i++;
+
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			i++;
+			return (i);
+		}
+		}
+	return (i);
+}
