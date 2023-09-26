297cad53f916a22ee72cbf4733910dc429859f5b
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-09-26 03:06:08 +0300

    revert task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (new == NULL)
 			return (NULL);
 		new->n = n;
-		if (*head)
+		if ((*head)->next)
 		{
 			new->next = (*head)->next;
 			(*head)->next = new;
