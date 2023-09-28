f80a2141f21fbee7429076567adbc68a6dddaaa7
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-09-28 22:04:59 +0300

    deleted redundant condition

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,11 +9,6 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i;
 
-	if (!n && !index)
-	{
-		return (0);
-	}
-
 	for (i = 0; i < index && n > 0; i++)
 		n >>= 1;
 	if (i == index)
