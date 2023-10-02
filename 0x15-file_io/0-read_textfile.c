d120954edf77a4b686ad61cc260a2153f4c9ec56
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-10-02 16:50:46 +0300

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * read_textfile - reads text file and prints to the POSIX standard output.
+ * @filename: name of file
+ * @letters: no. of letters to read and print
+ * Return: 0 if filename is NULL || file cannot be opened or read or
+ *			write fails or doesnt write the expected no. of bytes
+ *			number of letters read otherwise (success)
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE* f;
+	char* s = NULL;
+
+	s = malloc(sizeof(*s) * letters + 1);
+
+	if ((f = fopen(filename, "r")) && s)
+	{
+		if(!fread(s, 1, letters, f))
+			return (0);
+		fclose(f), free(s);
+		return (write(1, s, _strlen(s)));
+	}
+
+	return (0);
+
+
+}
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: pointer to a string.
+ * Return: length of string s.
+ */
+int _strlen(char *s)
+{
+	int  l = 0;
+
+	while (s[l])
+		l++;
+	return (l);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#ifndef BUFF
+#define BUFF 1024
+#endif
+
+/* Task functions */
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+/* Utility */ 
+
+int _strlen(char *s);
+#endif
