d640324ec2b891b30b04e217e8ecae974ddcdecc
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-10-02 18:08:04 +0300

    checking is signal/status is necessary

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int  len = 0, status = 1;
+	int  len = 0;
 
 	if (!filename)
 		return (-1);
@@ -25,11 +25,11 @@ int create_file(const char *filename, char *text_content)
 		if (text_content)
 		{
 			len = strlen(text_content);
-			status = write(fd, text_content, len);
+		write(fd, text_content, len);
 		}
 		close(fd);
 
-		return (status >= 0 ? 1 : -1);
+		return (1);
 	}
 
 	return (-1);
