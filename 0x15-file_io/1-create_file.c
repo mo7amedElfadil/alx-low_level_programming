d1a9b24c353824daf9c0eb89de839a7dc58740c6
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-10-02 18:01:39 +0300

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,8 @@ int create_file(const char *filename, char *text_content)
 	if (fd > 0)
 	{
 		len = strlen(text_content);
-		write(fd, text_content, len);
+		if (text_content)
+			write(fd, text_content, len);
 		close(fd);
 		return (1);
 	}
