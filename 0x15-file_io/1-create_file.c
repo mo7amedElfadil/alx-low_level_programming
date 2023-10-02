d9a601254c78e41fdb42b2f9ac230559a9c818da
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-10-02 18:07:17 +0300

    moved strlen into if statement and used a signal to determine return of write

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int  len = 0;
+	int  len = 0, status = 1;
 
 	if (!filename)
 		return (-1);
@@ -22,11 +22,14 @@ int create_file(const char *filename, char *text_content)
 			S_IRUSR | S_IWUSR);
 	if (fd > 0)
 	{
-		len = strlen(text_content);
 		if (text_content)
-			write(fd, text_content, len);
+		{
+			len = strlen(text_content);
+			status = write(fd, text_content, len);
+		}
 		close(fd);
-		return (1);
+
+		return (status >= 0 ? 1 : -1);
 	}
 
 	return (-1);
