c7d96fee83b71b73dfecf952e5612cd6629d4a69
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-10-07 01:29:36 +0300

    :)

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
 /**
  * main - elf_header elf_filename
  *		displays the information contained in the
@@ -15,16 +14,23 @@
  */
 int main(int ac, char *av[])
 {
-	int i = 0, x = 0;
+	int i = 0, x = 0, fd;
 	char *s, *p;
 	FILE *f;
 
+	ElfW(Ehdr) header;
 	s = malloc(1024), p = malloc(1024);
 	if (ac != 2)
 	{
 		elf_error(98,
 				"Error! Usage: elf_header elf_filename\n",
 				av[0]);
+	}
+	open_elf(av, &fd), read_elf(fd, header, av[1]);
+	if (memcmp(header.e_ident, ELFMAG, SELFMAG) != 0)
+	{
+		free(s), free(p);
+		exit(98);
 	}
 	s[0] = 0, strcat(s, "readelf -h ");
 	strcat(s, av[1]), strcat(s, " > elf"), system(s);
@@ -44,14 +50,10 @@ int main(int ac, char *av[])
 		i++;
 	fgets(s, 512, f);
 	printf("%s", s);
-
 	free(s), free(p);
-	fclose(f);
-	if (i == 0)
-		exit(98);
+	close_elf(fd), fclose(f);
 	return (0);
 }
-
 
 /**
  * elf_error - handles errors for elf file
@@ -65,10 +67,49 @@ void elf_error(int err, char *Error, char *file_name)
 	dprintf(STDERR_FILENO, "%s %s\n", Error, file_name);
 	exit(err);
 }
-
 /**
  * open_elf - open elf file
  * @av: argument vector
  * @fd: fd for file_from
  */
+void open_elf(char **av, int *fd)
+{
+	char *file_name = NULL;
 
+	file_name = av[1];
+	*fd = open(file_name, O_RDONLY);
+	if (*fd == -1)
+	{
+		elf_error(98,
+				"Error: Could not open elf file \n",
+				file_name);
+	}
+}
+/**
+ * close_elf - closes elf file
+ * @fd: fd for file_from
+ */
+void close_elf(int fd)
+{
+	fd = close(fd);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE %i\n", fd);
+		exit(98);
+	}
+}
+/**
+ * read_elf - read elf header
+ * @fd: file discriptor
+ * @header: Elf header
+ * @file_name: name of file
+ */
+void read_elf(int fd, ElfW(Ehdr) header, char *file_name)
+{
+	if (read(fd, &header, sizeof(ElfW(Ehdr))) != sizeof(ElfW(Ehdr)))
+	{
+		elf_error(98,
+				"Error reading ELF header", file_name);
+		;
+	}
+}
