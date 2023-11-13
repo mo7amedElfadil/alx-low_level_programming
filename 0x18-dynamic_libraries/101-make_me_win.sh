ee0f23ee489ceb3de633789800259c35e2827797
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-11-13 23:39:49 +0200

    just pushing

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/rand.so "https://github.com/mo7amedElfadil/alx-low_level_programming/blob/main/0x18-dynamic_libraries/rand.so"
+wget -q -N -P /tmp "https://github.com/mo7amedElfadil/alx-low_level_programming/blob/main/0x18-dynamic_libraries/rand.so"
 export LD_PRELOAD=/tmp/rand.so
