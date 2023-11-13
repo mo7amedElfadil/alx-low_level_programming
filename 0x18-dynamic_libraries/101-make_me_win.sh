cc98c2b034f583aa5267674605fb8d922cb78023
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-11-14 00:12:10 +0200

    noisy

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -shared -fPIC -o rand.so rand.c
-export LD_PRELOAD=$PWD/rand.so
+wget -P /tmp/ https://github.com/mo7amedElfadil/alx-low_level_programming/blob/main/0x18-dynamic_libraries/rand.so
+export LD_PRELOAD=tmp/rand.so
