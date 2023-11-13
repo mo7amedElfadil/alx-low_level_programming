5f18325fd796333d94132126fdcf4369f847aecd
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-11-14 00:05:30 +0200

    trying curl

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -P /tmp/ https://github.com/mo7amedElfadil/alx-low_level_programming/blob/main/0x18-dynamic_libraries/rand.so
-export LD_PRELOAD=/tmp/rand.so
+curl -O  https://github.com/mo7amedElfadil/alx-low_level_programming/blob/main/0x18-dynamic_libraries/rand.so
+export LD_PRELOAD=/rand.so
