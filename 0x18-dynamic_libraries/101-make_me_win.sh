0ef9c5740a70303e91ffcab74d6f889efbdf726c
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-11-14 00:19:01 +0200

    forgot a forward slash

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/mo7amedElfadil/alx-low_level_programming/raw/main/0x18-dynamic_libraries/rand.so
-export LD_PRELOAD=tmp/rand.so
+wget -P /tmp https://github.com/mo7amedElfadil/alx-low_level_programming/raw/main/0x18-dynamic_libraries/rand.so
+export LD_PRELOAD=/tmp/rand.so
