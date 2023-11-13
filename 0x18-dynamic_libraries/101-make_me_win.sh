e51f07a18717675a5514f2d4e479df31bcba8e47
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-11-13 23:16:41 +0200

    changed download location to tmp so it gets autodeleted

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q https://github.com/mo7amedElfadil/alx-low_level_programming/blob/main/0x18-dynamic_libraries/rand.so
-export LD_PRELOAD="$PWD/rand.so"
+wget -q -P -o /tmp https://github.com/mo7amedElfadil/alx-low_level_programming/blob/main/0x18-dynamic_libraries/rand.so
+export LD_PRELOAD="/tmp/rand.so"
