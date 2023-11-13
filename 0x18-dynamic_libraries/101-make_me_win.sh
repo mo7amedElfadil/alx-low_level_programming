eb6ae965321f0914d1147f21c0831ad5ba4d27d8
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-11-14 00:14:31 +0200

    fixed link

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/mo7amedElfadil/alx-low_level_programming/blob/main/0x18-dynamic_libraries/rand.so
+wget -q -P /tmp/ https://github.com/mo7amedElfadil/alx-low_level_programming/raw/main/0x18-dynamic_libraries/rand.so
 export LD_PRELOAD=tmp/rand.so
