b39a190828091853853d7fa4f32a0a4b4f443079
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-11-13 23:11:51 +0200

    repushing

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/mo7amedElfadil/alx-low_level_programming/blob/main/0x18-dynamic_libraries/rand.so
-export LD_PRELOAD=$PWD/rand.so
+wget -q https://github.com/mo7amedElfadil/alx-low_level_programming/blob/main/0x18-dynamic_libraries/rand.so
+export LD_PRELOAD="$PWD/rand.so"
