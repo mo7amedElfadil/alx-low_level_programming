7182074ab261d9d95c2e5e6c74c41e57e47fc1d1
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-11-30 20:58:57 +0200

    3-hash_table_set.c searching for dups and using temp value to test for val

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+#include <stdio.h>
 #include <string.h>
 /**
  * new_node - creates a new element node
@@ -9,6 +10,7 @@
 hash_node_t *new_node(const char *key, const char *value)
 {
 	hash_node_t *new = malloc(sizeof(hash_node_t *));
+
 	if (!new)
 		return (0);
 	new->key = strdup(key);
@@ -44,14 +46,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!ht || !key || !ht->array || !ht->size || !*key || !value)
 		return (0);
 	idx = key_index((unsigned char *)key, ht->size);
-	for (search = ht->array[idx]; search && search->next ; search = search->next)
+	for (search = ht->array[idx]; search; search = search->next)
 	{
 		if (!strcmp(key, search->key))
 		{
-			free(search->value);
-			search->value = strdup(value);
-			if (!search->value)
+			char *new_val = strdup(value);
+
+			if (!new_val)
 				return (0);
+			free(search->value);
+			search->value = new_val;
 			return (1);
 		}
 	}
