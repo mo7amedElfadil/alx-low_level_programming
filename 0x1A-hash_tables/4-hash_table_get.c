598c84e845aa5f2d4777081127a2be4c6721c641
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-11-30 21:10:01 +0200

    4-hash_table_get.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves value associated with key
+ * @ht: pointer to hash table
+ * @key: key string
+ * Return: value if found / NULL if not
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node = ht->array[key_index((unsigned char *)key, ht->size)];
+
+	if (!node)
+		return (NULL);
+	while (node && strcmp(node->key, key))
+		node = node->next;
+	if (!node)
+		return (NULL);
+	return (node->value);
+}
