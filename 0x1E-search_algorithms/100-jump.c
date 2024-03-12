666558052bf3b8f8b5fea03a4301a79181f10177
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-03-12 20:01:48 +0200

    eq size

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -17,7 +17,7 @@ int jump_search(int *array, size_t size, int value)
 	for (i = 0; i < (int)size; i += jump)
 	{
 		printf("Value checked array[%i] = [%i]\n", i, array[i]);
-		if (i + jump > (int)size || array[i + jump] >= value)
+		if (i + jump >= (int)size || array[i + jump] >= value)
 			return (jump_search_helper(array, i, i + jump, size, value));
 	}
 	return (-1);
