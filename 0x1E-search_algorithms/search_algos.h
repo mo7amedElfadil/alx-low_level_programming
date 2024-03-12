d931f3c7d1755b90af93e873a3b57f38bb1be7b4
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-03-12 19:45:15 +0200

    bet

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -35,10 +35,10 @@ typedef struct listint_s
  */
 typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
 } skiplist_t;
 
 /* ------------ Prototype declarations ------------ */
